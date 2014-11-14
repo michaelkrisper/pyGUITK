ca36a0fc4800c26b8d0137201c1cf185c36ca411
Michael Krisper <michael.krisper@gmail.com>
2014-11-14 15:15:57 +0100

    Initial commit

---
+++ bucky.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# coding=utf-8
+"""
+GUI Demo
+https://www.youtube.com/watch?v=RJB1Ek2Ko_Y
+"""
+
+__author__ = "Michael Krisper"
+__email__ = "michael.krisper@gmail.com"
+__date__ = "2014-11-14"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
